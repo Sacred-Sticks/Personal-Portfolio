f9575007bb2e8cec4f65e9f49fb3eb3b99e8b7c9
Lucas Ackman <AckmanLD@gmail.com>
2023-05-21 15:48:13 -0500

    Added empty Projects and Panels for activation

--- Portfolio Page/Scripts/script.js
+++ Portfolio Page/Scripts/script.js
@@ -1,5 +1,16 @@
 const consoleElement = document.getElementById('console');
 const commands = ["overview", "projects", "open", "contact", "socials"];
+const projects = ["kickstarter", "blacksite-theta", "where-silence-survives"]
+const overview = document.getElementById('overview');
+const contact = document.getElementById('contact');
+const socials = document.getElementById('socials');
+const kickstarter = document.getElementById('p.kickstarter');
+const blacksiteTheta = document.getElementById('p.blacksite-theta');
+const whereSilenceSurvives = document.getElementById('p.where-silence-survives');
+const buttons = document.querySelectorAll('.window-closer');
+const panelMap = new Map();
+const projectMap = new Map();
+var panels;
 var inputElement;
 
 function handleCommand(command) {
@@ -8,36 +19,60 @@ function handleCommand(command) {
 
   commands.forEach(option => {
     if (action === `/${option}`) {
-      printOverview();
-        commandRecognized = true;
+    commandRecognized = true;
+      if (option === commands[1])
+      {
+        printAllProjects();
+        return;
+      }
+      const value = GetTarget(option, args);
+      if (!value)
+        return;
+
+      value.classList.remove('hidden');
+      inputElement.disabled = true;
       return;
     }
   });
 
   if (!commandRecognized)
-    printToConsole('Command Not Recognized', 0);
+    printToConsole('Command Not Recognized');
 }
 
-function printToConsole(output, paddingLevel) {
+function GetTarget(option, args) {
+  if (option === commands[2])
+  {
+    if (projectMap.has(args[0]))
+      return projectMap.get(args[0]);
+    else {
+      printToConsole(`${args[0]} could not be found`);
+      return;
+    }
+  }
+  else {
+    return panelMap.get(option);
+  }
+}
+
+function printToConsole(output) {
   const outputElement = document.createElement('div');
   outputElement.classList.add('row');
-  outputElement.classList.add(`py-${paddingLevel}`);
   outputElement.textContent = output;
   consoleElement.prepend(outputElement);
 }
 
-function printAllToConsole(outputLines, delay) {
-  let index = 0;
+function printAllProjects() {
+  var index = 0;
 
-  function printNextLine() {
-    if (index < outputLines.length) {
-      printToConsole(outputLines[index], 2);
-      index++;
-      setTimeout(printNextLine, delay);
-    }
+  function writeNextLine() {
+    if (index == projects.length)
+      return;
+    printToConsole(projects[index]);
+    index++;
+    setTimeout(writeNextLine, 500);
   }
 
-  printNextLine();
+  writeNextLine();
 }
 
 function handleInput(event) {
@@ -48,7 +83,7 @@ function handleInput(event) {
   if (command === '')
     return;
   event.target.value = '';
-  printToConsole(`> ${command}`, 0);
+  printToConsole(`> ${command}`);
   inputElement.blur();
   setTimeout(() => {
     handleCommand(command);
@@ -68,7 +103,7 @@ function showCommands() {
     commandRow.classList.add('row');
     const commandElement = document.createElement('div');
     commandElement.classList.add('text-center');
-    commandElement.classList.add('text-primary')
+    commandElement.classList.add('text-secondary')
     commandElement.classList.add('py-2');
     commandElement.textContent = "/" + command;
     commandRow.appendChild(commandElement);
@@ -76,43 +111,34 @@ function showCommands() {
   });
 }
 
-function printOverview() {
-  const textToPrint = ["Lucas Ackman",
-  "Software Engineer | Game Developer",
-  "Hi, I'm Lucas Ackman, a software engineer and game developer specializing in "
-  + "mathematical programming. Despite being born with a physical disability that "
-  + "requires crutches for mobility, my love for computers and math led me to the "
-  + "fascinating world of programming.",
-  "Throughout my journey, I've completed several noteworthy projects:",
-  "Kickstarter: Developed a Unity package that kickstarts projects by providing "
-  + "an input manager and scriptable objects for efficient code decoupling. This "
-  + "project allowed me to delve into the SOLID design principles and strengthen "
-  + "my architectural skills.",
-  "Blacksite Theta: Created a captivating puzzle-platformer game as a university "
-  + "capstone project, where I embraced functional programming techniques.",
-  "Where Silence Survives: Designed a groundbreaking virtual reality game, "
-  + "challenging conventions by exploring the concept of blindness and echolocation.",
-  "During my studies at the University of Wisconsin-Whitewater, I acquired proficiency "
-  + "in C#, C++, Java, and the implementation of advanced systems like artificial "
-  + "intelligence and pathfinding using various data structures. I've also realized "
-  + "the immense power of functional programming and how it can transform the "
-  + "gaming experience into something truly unique.",
-  "With my personal background, having spent much of my childhood immersed in video "
-  + "games, I'm driven by the desire to bring joy to others through my creations. "
-  + "I aim to create games that not only entertain but also subtly educate players, "
-  + "providing a seamless blend of enjoyment and learning.",
-  "My approach to development revolves around adhering to the SOLID design principles, "
-  + "enabling efficient bug identification and resolution. Additionally, I possess "
-  + "a unique perspective on accessibility, particularly for games incorporating "
-  + "physicality components, as I can provide insights on gameplay experiences "
-  + "from my own disabled perspective.",
-  "If you believe I could be a valuable addition to your team, please don't "
-  + "hesitate to reach out to me via the contact form!"];
+function assignPanels() {
+  panels = [overview, contact, socials, kickstarter];
+  panelMap.set(commands[0], overview);
+  panelMap.set(commands[3], contact);
+  panelMap.set(commands[4], socials);
+  panelMap.set(`${commands[2]} kickstarter`, kickstarter);
+}
 
-  printAllToConsole(textToPrint.reverse(), 250);
+function assignProjects() {
+  projectMap.set(projects[0], kickstarter);
+  projectMap.set(projects[1], blacksiteTheta);
+  projectMap.set(projects[2], whereSilenceSurvives);
+}
+
+function assignButtons() {
+  buttons.forEach(button => {
+    button.onclick = function() {
+      button.parentElement.parentElement.parentElement.classList.add('hidden');
+      inputElement.disabled = false;
+      inputElement.focus();
+    }
+  });
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   assignCommandListener();
+  assignPanels();
+  assignProjects();
   showCommands();
+  assignButtons();
 });
