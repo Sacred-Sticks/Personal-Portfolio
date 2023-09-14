161e09c321da84a3f6971e78bb99fe4b536c9e1d
Lucas Ackman <AckmanLD@gmail.com>
2023-09-13 20:43:53 -0500

    Implemented all project JSONs into the webpage

--- Scripts/LayOutProjects.js
+++ Scripts/LayOutProjects.js
@@ -1,10 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
     const highlightedProjectsRoot = document.getElementById("Highlighted-Projects");
-    
+    const professionalProjectsRoot = document.getElementById("Professional-Projects");
+    const personalProjectsRoot = document.getElementById("Personal-Projects");
+    const gameJamProjectsRoot = document.getElementById("Game-Jam-Projects");
+    const classProjectsRoot = document.getElementById("Class-Work-Projects")
+
     fetch("https://raw.githubusercontent.com/Sacred-Sticks/sacred-sticks.github.io/main/Scripts/HighlightedProjects.json")
         .then(response => response.json())
-        .then(projects => 
+        .then(projects =>
             projects.forEach(project => AddProject(highlightedProjectsRoot, project)));
+    fetch("https://raw.githubusercontent.com/Sacred-Sticks/sacred-sticks.github.io/main/Scripts/ProfessionalProjects.json")
+        .then(response => response.json())
+        .then(projects =>
+            projects.forEach(project => AddProject(professionalProjectsRoot, project)));
+    fetch("https://raw.githubusercontent.com/Sacred-Sticks/sacred-sticks.github.io/main/Scripts/PersonalProjects.json")
+        .then(response => response.json())
+        .then(projects =>
+            projects.forEach(project => AddProject(personalProjectsRoot, project)));
+    fetch("https://raw.githubusercontent.com/Sacred-Sticks/sacred-sticks.github.io/main/Scripts/GameJamProjects.json")
+        .then(response => response.json())
+        .then(projects =>
+            projects.forEach(project => AddProject(gameJamProjectsRoot, project)));
+    fetch("https://raw.githubusercontent.com/Sacred-Sticks/sacred-sticks.github.io/main/Scripts/ClassWorkProjects.json")
+        .then(response => response.json())
+        .then(projects =>
+            projects.forEach(project => AddProject(classProjectsRoot, project)));
 });
 
 function AddProject(rootElement, project) {
@@ -16,7 +36,7 @@ function AddProject(rootElement, project) {
     const title = document.createElement("h5");
     const description = document.createElement("p");
 
-    container.classList.add("col-lg-4", "col-md-6", "text-primary");
+    container.classList.add("col-lg-4", "col-md-6", "text-primary", "mb-3");
 
     card.classList.add("card", "bg-secondary", "m-2");
 
